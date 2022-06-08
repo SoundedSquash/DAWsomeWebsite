252bc6bc5d9f012d0827596b81c33e673c9c6dae
SoundedSquash <20405599+SoundedSquash@users.noreply.github.com>
2022-06-08 01:00:37 -0500

    Needs to be refactored a lot, but added features. Adds a new line for next lyric. Removes last line if previous line is empty.

---
+++ SongLyrics/Data/CallbackHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace SongLyrics.Data
+{
+    public class CallbackHelper
+    {
+        public class LyricChangeCallbackArgs
+        {
+            public int Id { get; set; }
+            public string? Lyric { get; set; }
+        }
+    }
+}
